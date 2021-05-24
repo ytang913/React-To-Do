0f101bf141fe5537927aef990a2197d8401abbba
ytang913 <ytang18@terpmail.umd.edu>
2021-05-24 13:53:44 -0700

    updating

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,10 +1,35 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import {useState} from 'react'
 
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors appointment',
+        day: 'Feb 5th at 2:30 pm',
+        reminder: 'true',
+    },
 
-function App() {
+    {
+        id: 2,
+        text: "Meeting at school",
+        day: 'Feb 6th at 1:30 pm',
+        reminder: 'true',
+    },
+
+    {
+        id: 3,
+        text: "Food Shopping",
+        day: 'Feb 21st at 3:30 pm',
+        reminder: 'false',
+    }
+])
+
   return (
     <div className="container">
-      <Header />     
+      <Header />   
+      <Tasks tasks = {tasks} />  
     </div>
   );
 }
--- todo/src/components/Tasks.js
+++ todo/src/components/Tasks.js
@@ -1,30 +1,12 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors appointment',
-        day: 'Feb 5th at 2:30 pm',
-        reminder: 'true',
-    },
 
-    {
-        id: 2,
-        text: "Meeting at school",
-        day: 'Feb 6th at 1:30 pm',
-        reminder: 'true',
-    }
 
-    {
-        id: 3,
-        text: "Food Shopping",
-        day: 'Feb 21st at 3:30 pm',
-        reminder: 'false',
-    }
-]
-
-const Tasks = () =>{
+const Tasks = ({ tasks }) =>{
+    
     return(
         <>
-        {tasks.map()}
+        {tasks.map((task) => (
+            <h3 key = {task.id}>{task.text}</h3>
+            ))}
         </>
     )
 }
