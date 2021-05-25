f76abc538dfd8efbac8d90cd8204946382dc1311
ytang913 <ytang18@terpmail.umd.edu>
2021-05-25 15:24:22 -0700

    updating

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -27,7 +28,7 @@ const App = () => {
     }
 ])
 // Add Task
-const AddTask = (task) => {
+const addTask = (task) => {
   const id = Math.floor(Math.random() * 10000)+1
 
   const newTask = { id , ...task }
@@ -47,10 +48,11 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header /> 
-      <AddTask onAdd = {addTask}/>  
-      {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder}/> 
-      : 'No Tasks to show'}  
+      <Header onAdd = {() => setShowAddTask(!showAddTask)} /> 
+      {showAddTask && <AddTask onAdd = {addTask}/>}
+
+      {tasks.length > 0 ? (<Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder}/>) 
+      : ('No Tasks to show')}  
     </div>
   );
 }
