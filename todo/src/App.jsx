3e07446a41f102ea87410e24f56ec692ca922eda
ytang913 <ytang18@terpmail.umd.edu>
2021-05-25 19:19:37 -0700

    Update App.jsx

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -48,7 +48,7 @@ const toggleReminder = (id) => {
 
   return (
     <div className= "container">
-      <Header onAdd = {() => setShowAddTask(!showAddTask)} /> 
+      <Header onAdd = {() => setShowAddTask(!showAddTask)} showAddTask = {showAddTask} /> 
       {showAddTask && <AddTask onAdd = {addTask}/>}
 
       {tasks.length > 0 ? (<Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder}/>) 
